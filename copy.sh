efa2a17bd74f06594749faef9e8fc630081de97c
Kclamberth <127368340+Kclamberth@users.noreply.github.com>
2023-09-15 22:18:06 -0700

    Update copy.sh

--- copy.sh
+++ copy.sh
@@ -1,21 +1,22 @@
 #!/bin/bash
 
 counter=1
+e0=$(pwd)
 
-for dir in /data/jellyfin/shows/*
+for dir in $e0/shows/*
 do
 	cd $dir
 	e1=$(ls -l | wc -l) #counts number of items in directory BEFORE yt-dlp runs
 
 	#downloads everything on a youtube channel that is NOT already in the archive.txt 
-	yt-dlp $(cat /data/jellyfin/channels.txt | sed -n "$counter"p) --download-archive archive.txt
+	yt-dlp $(cat $e0/channels.txt | sed -n "$counter"p) --download-archive archive.txt
 
 	e2=$(ls -l | wc -l) #counts number of items in directory AFTER yt-dlp runs
 
 	#grabs the first video in the folder and stores it's name in a variable
 	e3=$(ls -lt | grep webm | head -n 1 | awk -F " " '{ for (i=9; i<=NF; i++) print $i }' | paste -s -d " ")
 
-	cd /data/jellyfin
+	cd $e0
 
 	if [ $e2 -gt $e1 ] #if items in directory grew after yt-dlp runs
 	then
