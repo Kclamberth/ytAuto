f98c2602794c7e6f4b7f07275bcb3d819be9772a
Kclamberth <127368340+Kclamberth@users.noreply.github.com>
2023-09-15 19:50:47 -0700

    Update copy.sh

--- copy.sh
+++ copy.sh
@@ -12,9 +12,13 @@ e2=$(ls -l | wc -l) #counts number of items in directory AFTER yt-dlp runs
 #grabs the first video in the folder and stores it's name in a variable
 e3=$(ls -lt | grep webm | head -n 1 | awk -F " " '{ for (i=9; i<=NF; i++) print $i }' | paste -s -d " ")
 
-if [ $e2 -ge $e1 ] #if items in directory grew after yt-dlp runs
+if [ $e2 -gt $e1 ] #if items in directory grew after yt-dlp runs
 then
-    echo $e3 downloaded on $(date) > lastupdated.txt
+    echo $e3 downloaded on $(date) >> lastupdated.txt
+else
+    echo No new videos as of $(date) >> lastupdated.txt
 fi  
+
+cat lastupdated.txt | tail -1 #prints out what changed since the script last ran
 
 
