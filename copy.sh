55c19e3102d5c0dedd2a485b19536a8d69827987
Kclamberth <127368340+Kclamberth@users.noreply.github.com>
2023-09-15 18:27:23 -0700

    Update copy.sh

--- copy.sh
+++ copy.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 cd ~/Videos/videodirectory
-yt-dlp $(cat ~/Videos/channels.txt) | grep 'has already been downloaded' >> /dev/null 2>&1
+yt-dlp $(cat ~/Videos/channels.txt) --download-archive archive.txt 
 e1=$?
 
-if [ $e1 -ne 0 ]
+if [ $e1 -eq 0 ]
 then
     echo New Video downloaded on $(date) > lastupdated.txt
 fi  
