412d5af2985f67091ad1ba9309cd59f632fea99b
Kclamberth <127368340+Kclamberth@users.noreply.github.com>
2024-03-09 02:01:27 -0800

    Create discordbot.sh

---
+++ discordbot.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+#command to run
+output="$(cat /media/youtube/lastupdated.txt | tail -n"$(cat /media/youtube/channels.txt | wc -l)")"
+
+#Discord Webhook URL: find in server settings
+webhook_url='[INSERT WEBHOOKS URL HERE]'
+
+#Format as JSON
+json_payload=$(jq -n --arg content "$output" '{content: $content}')
+
+#send to discord
+curl -H "Content-Type: application/json" -X POST -d "$json_payload" $webhook_url
