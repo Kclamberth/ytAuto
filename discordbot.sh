e512bb8c8c28087ef5367f4469f85f215d13b25d
Kclamberth <127368340+Kclamberth@users.noreply.github.com>
2024-03-15 01:18:15 -0700

    Update discordbot.sh

--- discordbot.sh
+++ discordbot.sh
@@ -4,14 +4,16 @@
 output="$(cat /media/youtube/lastupdated.txt | tail -n"$(cat /media/youtube/channels.txt | wc -l)" | grep "downloaded")"
 
 if [[ -z "$output" ]]; then
-	output="Update: All channels scanned. No new content detected as of $(date '+%b-%d-%Y %H:%M:%S')."
+	output="All channels scanned. No new content detected as of $(date '+%b-%d-%Y %H:%M:%S')."
 fi
+
+formattedOutput=$'```Update:\n'"${output}"$'\n```'
 
 #Discord Webhook URL: find in server settings
 webhook_url='[INSERT WEBHOOKS URL HERE]'
 
 #Format as JSON
-json_payload=$(jq -n --arg content "$output" '{content: $content}')
+json_payload=$(jq -n --arg content "$formattedOutput" '{content: $content}')
 
 #send to discord
 curl -H "Content-Type: application/json" -X POST -d "$json_payload" $webhook_url
