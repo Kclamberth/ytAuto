b76449d85026768449bea3ebd0ed67ecb8621b5c
Kclamberth <127368340+Kclamberth@users.noreply.github.com>
2024-03-15 01:20:29 -0700

    Update copy.sh

--- copy.sh
+++ copy.sh
@@ -39,9 +39,9 @@ while IFS= read -r line; do
     afterDownload=$(ls -A | wc -l)
 
     if [ "$afterDownload" -gt "$beforeDownload" ]; then
-        echo "New content downloaded for ${channelName} on $(date '+%b-%d-%Y %H:%M:%S')." >> "${youtubeDir}/lastupdated.txt"
+        echo "${channelName} scan complete. New content archived on $(date '+%b-%d-%Y %H:%M:%S')." >> "${youtubeDir}/lastupdated.txt"
     else
-        echo "No new videos for ${channelName} as of $(date '+%b-%d-%Y %H:%M:%S')." >> "${youtubeDir}/lastupdated.txt"
+        echo "${channelName} scan complete. No new content as of $(date '+%b-%d-%Y %H:%M:%S')." >> "${youtubeDir}/lastupdated.txt"
     fi
 
     ((counter++))
