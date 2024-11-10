8cc3ba9cf23b2e710a13be91426703df2f7ef0c1
Kclamberth <127368340+Kclamberth@users.noreply.github.com>
2024-11-10 15:48:39 -0800

    Add files via upload

--- ytAuto.c
+++ ytAuto.c
@@ -1,11 +1,8 @@
 #include <dirent.h>
-#include <linux/limits.h>
-#include <stdbool.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #include <sys/stat.h>
-#include <sys/types.h>
 #include <time.h>
 #include <unistd.h>
 #include <wait.h>
