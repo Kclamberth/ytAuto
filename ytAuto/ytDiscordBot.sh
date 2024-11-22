139b892ffc71a2c8a061acbd0c6fd98b921c2c03
Kclamberth <127368340+Kclamberth@users.noreply.github.com>
2024-11-22 14:26:13 -0800

    Update ytDiscordBot.sh

--- ytAuto/ytDiscordBot.sh
+++ ytAuto/ytDiscordBot.sh
@@ -5,23 +5,21 @@ YOUTUBE_DIR="${BASE_DIR}/youtube"
 UPDATE_DATE=$(TZ='America/Los_Angeles' date '+%b-%d-%Y')
 WEBHOOK_URL='{WEBHOOK_URL}'
 
-# Read updates into an array
 IFS=$'\n' read -d '' -r -a updates < <(cat "$YOUTUBE_DIR"/.channels.log | tail -n"$(grep -cv '^$' "$YOUTUBE_DIR"/.channels.list)" | grep "NEW" && printf '\0')
 
 if [ ${#updates[@]} -eq 0 ]; then
-    # No updates found
+    # no updates found
     output="[$(TZ='America/Los_Angeles' date '+%H:%M:%S')] All channels scanned. No changes."
 else
-    # Process updates to format as requested
+    # process updates to format 
     for i in "${!updates[@]}"; do
         updates[i]=$(echo "${updates[i]}" | sed 's/\[[0-9]*-[0-9]*-[0-9]* \([0-9]*:[0-9]*:[0-9]*\)\]/[\1]/')
     done
 
-    # Join updates with newline character
     output=$(printf '%s\n' "${updates[@]}")
 fi
 
-# Format as JSON
+# JSON
 json_payload=$(jq -n --arg title "**Update $UPDATE_DATE:**" --arg desc "$output" --argjson color 5814783 \
   '{embeds: [{title: $title, description: $desc, color: $color}]}')
 
