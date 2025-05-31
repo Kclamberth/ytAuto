a78cabe374ebd7acfed93ee75f17e70cf9d8d363
kclamberth <kclamberth@protonmail.com>
2025-05-31 14:39:57 -0700

    Modified fork_process to take log path

--- include/process.h
+++ include/process.h
@@ -2,10 +2,7 @@
 #define PROCESS_H
 #include <stdio.h>
 
-void fork_process(const char *full_link,
-                  const char *channel_dir,
-                  const char *channel_name,
-                  FILE       *log_file);
+void fork_process(const char *full_link, const char *channel_dir,
+                  const char *channel_name, const char *log_path);
 
 #endif
-
