4630b8deb65a64aa5df5cb84d146bfb09cd4ec45
kclamberth <kclamberth@protonmail.com>
2025-05-31 15:13:09 -0700

    Fixed log lock to ensure exclusitivity

--- src/log.c
+++ src/log.c
@@ -1,6 +1,7 @@
 #include "../include/log.h"
 #include "../include/config.h"
 #include <stdio.h>
+#include <sys/file.h>
 #include <time.h>
 
 void log_line(const char *log_path, const char *line) {
@@ -10,16 +11,25 @@ void log_line(const char *log_path, const char *line) {
     return;
   }
 
-  // setup time structure
+  // Lock log file
+  int fd = fileno(log_file);
+  if (flock(fd, LOCK_EX) != 0) {
+    perror("log_line: flock");
+    fclose(log_file);
+    return;
+  }
+
+  // Setup time structure
   time_t timestamp = time(NULL);
   struct tm datetime = *localtime(&timestamp);
   char time_output[TIME_OUTPUT_SIZE];
   strftime(time_output, sizeof(time_output), "%Y-%m-%d %H:%M:%S", &datetime);
 
-  // lock log, write to it, unlock
-  flockfile(log_file);
+  // Write to log
   fprintf(log_file, "[%s] %s\n", time_output, line);
-  funlockfile(log_file);
   fflush(log_file);
+
+  // Unlock log file & close
+  flock(fd, LOCK_UN);
   fclose(log_file);
 }
