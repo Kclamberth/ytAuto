e456d912a2a7f6b72bb2f9d6cf71ce4d852096fd
Kclamberth <127368340+Kclamberth@users.noreply.github.com>
2025-06-30 19:23:15 -0700

    Added timeout to ytdlp process

--- src/process.c
+++ src/process.c
@@ -3,8 +3,10 @@
 #include "../include/log.h"
 #include "../include/ytdlp_download.h"
 #include <linux/limits.h>
+#include <signal.h>
 #include <stdio.h>
 #include <sys/wait.h>
+#include <time.h>
 #include <unistd.h>
 
 void fork_process(const char *full_link, const char *channel_dir,
@@ -35,7 +37,28 @@ void fork_process(const char *full_link, const char *channel_dir,
     }
 
     // Child waits for grandchild to finish checking count again
-    waitpid(ytdlp, NULL, 0);
+    time_t start = time(NULL);
+    int status;
+
+    while (1) {
+      pid_t done = waitpid(ytdlp, &status, WNOHANG);
+      if (done == ytdlp)
+        break;
+
+      if (difftime(time(NULL), start) > TIMEOUT_SEC) {
+        kill(ytdlp, SIGTERM);
+        sleep(5);
+        kill(ytdlp, SIGKILL);
+
+        char msg[LOG_MSG_SIZE];
+        snprintf(msg, sizeof(msg), "[T/O] %s timed out after %d sec",
+                 channel_name, TIMEOUT_SEC);
+        log_line(log_path, msg);
+        _exit(1);
+      }
+      sleep(1);
+    }
+
     int after = count_lines(archive_path);
 
     // Log message
